018c1ca2db39452941c95a6c5b02b7e475c081fb
WilCahuaya <48217068@continental.edu.pe>
2022-09-11 16:06:13 -0500

    START SPINNER

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSpinner=findViewById(R.id.btnSpinner);
+
+        btnSpinner.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent iSpinner=new Intent(this,Spinner.class);
+        startActivity(iSpinner);
     }
 }
