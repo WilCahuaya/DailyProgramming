d82651dc74c794af1498c7b4ae123654566092ab
wilCahuaya <48217068@continental.edu.pe>
2022-09-12 14:54:03 -0500

    Menu complete

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnSpinner;
@@ -24,4 +27,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent iSpinner=new Intent(this,SpinnerEjm.class);
         startActivity(iSpinner);
     }
+
+    @Override
+    public  boolean onCreateOptionsMenu(Menu mimenu){
+        getMenuInflater().inflate(R.menu.menu_activity_ejm,mimenu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem opcio_menu){
+        int id=opcio_menu.getItemId();
+        if(id==R.id.itmSpine){
+            Intent iSpinner=new Intent(this,SpinnerEjm.class);
+            startActivity(iSpinner);
+            return true;
+        }
+        if(id==R.id.itmOpcion){
+            Toast.makeText(this,"Opción",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(opcio_menu);
+    }
+
 }
